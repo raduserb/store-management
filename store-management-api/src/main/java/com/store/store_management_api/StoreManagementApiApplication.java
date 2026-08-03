1a6b462d4f7b053fe80597c9f8eb262434f11f7f
raduserb <radu.serb06@gmail.com>
2026-08-03 21:15:15 +0300

    feat: Add project template

---
+++ store-management-api/src/main/java/com/store/store_management_api/StoreManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.store.store_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementApiApplication.class, args);
+	}
+
+}
